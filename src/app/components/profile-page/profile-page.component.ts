2a8348fc5f4742440f1c85480dd2f7ef5fc489f8
DaliusDeveikis <slkyper@gmail.com>
2022-07-13 11:36:42 +0300

    create profile design, use Two-way binding to edit text

--- src/app/components/profile-page/profile-page.component.ts
+++ src/app/components/profile-page/profile-page.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilePageComponent implements OnInit {
 
+  public UserFullName:string = 'Dalius Deveikis'
+  public UserInfo:string = 'JavaScript Developer'
+  public UserAbout:string = "Hello, I'm learning programming for the second year, I don't have much experience in official companies, I'm trying to improve myself. I am looking for a job, and I am also interested in an internship. I never give up, so I devote all my time to programming. 🤓 🌍 "
+  public canEditCode = false
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  
 }
